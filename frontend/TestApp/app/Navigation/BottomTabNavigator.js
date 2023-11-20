aeab8840b3d1afb4d55da02335aa07b2c8f89628
JohnMejia014 <johnmejia223@gmail.com>
2023-11-19 23:30:55 -0600

    ,

--- frontend/TestApp/app/Navigation/BottomTabNavigator.js
+++ frontend/TestApp/app/Navigation/BottomTabNavigator.js
@@ -7,7 +7,7 @@ import DiscoveryScreen from './DiscoveryScreen';
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => (
-  <Tab.Navigator>
+  <Tab.Navigator >
     <Tab.Screen name="Feed" component={FeedScreen} />
     <Tab.Screen name="Discovery" component={DiscoveryScreen} />
     <Tab.Screen name="Profile" component={ProfileScreen} />
