4958c76452726f2deaa38d4f4e31af8eabe07856
JohnMejia014 <johnmejia223@gmail.com>
2023-12-22 05:38:25 -0600

    added pictures to markers

---
+++ kickoff/kickoff/kickoffApp.swift
@@ -0,0 +1,17 @@
+//
+//  kickoffApp.swift
+//  kickoff
+//
+//  Created by John Mejia on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct kickoffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
