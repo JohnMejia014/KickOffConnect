b388a167a6c257aaf3b43751219f3ce55b0a0ec7
marcoflo02 <70395072+marcoflo02@users.noreply.github.com>
2024-03-06 14:40:46 -0600

    Update FeedScreen.js

--- frontend/TestApp/app/Navigation/FeedScreen.js
+++ frontend/TestApp/app/Navigation/FeedScreen.js
@@ -1,11 +1,44 @@
 // FeedScreen.js
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {View, Text, FlatList, TouchableOpacity, TextInput, ActivityIndicator, Image, Button} from 'react-native';
+import {NavigationContainer, useScrollToTop} from "@react-navigation/native";
+import {Link} from "expo-router";
+import Navigation from "./Navigation";
+import VideoPlayer  from "./FeedPageDiscovery/VideoPlayer";
+import ImageUpload from "./FeedPageDiscovery/ImageUpload";
+import StackNavigator from "@react-navigation/stack/src/navigators/createStackNavigator";
+import {ScreenStack} from "react-native-screens";
+import {createNativeStackNavigator} from "react-native-screens/native-stack";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import FeedList from "./FeedPageDiscovery/FeedList";
+import PostObject from "./FeedPageDiscovery/PostObject";
 
+<<<<<<< HEAD
+=======
 const FeedScreen = (userInfo) => (
   <View>
     <Text>Feed Screen</Text>
   </View>
 );
+>>>>>>> f1202e7f456ea3d88c4d4cb7938c4954fefd3758
+
+
+const Stack = createNativeStackNavigator();
+
+const FeedScreen = ({navigation}) => {
+
+    return(
+
+
+        <Stack.Navigator>
+            <Stack.Screen name="Root" component={FeedList} options={{headerShown: false, }}/>
+            <Stack.Screen name="Video" component={VideoPlayer} options={{headerShown: false }}/>
+            <Stack.Screen name="Create" component={PostObject} options={{headerShown: false}}/>
+        </Stack.Navigator>
+
+    )
+};
+
+
 
 export default FeedScreen;
